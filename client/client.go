11f0c5ee00f97af9660cdb8c724a9adbb982448e
Sk Shahriar Ahmed Raka <skshahriarahmedraka@gmail.com>
2023-07-25 08:46:22 +0600

    added:

--- client/client.go
+++ client/client.go
@@ -10,13 +10,12 @@
 package main
 
 import (
-	// "context"
-	"crypto/tls"
-	"flag"
+	"context"
+	// "crypto/tls"
+	// "flag"
 	"fmt"
-	"log"
-
-	"golang.org/x/net/context"
+	// "log"
+	// "net"
 
 	//"os"
 	"client/proto"
@@ -25,65 +24,19 @@ import (
 	"client/logs"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	// "google.golang.org/grpc/credentials"
 )
 
-// func Panicking(msg string , err error){
-// 	if err!= nil {
-// 		log.Fatalln(msg ," ", err)
-// 	}
-// }
 
-// func main () {
-// 	conn , err := grpc.Dial(":50051",grpc.WithInsecure(),grpc.WithBlock())
 
-// 	Panicking("error : ",err)
-// 	defer conn.Close()
 
-// 	c:=messagepb.NewConversationClient(conn)
-
-// 	name:= "raka"
-// 	ctx,cancel := context.WithTimeout(context.Background(),time.Second)
-// 	defer cancel()
-
-// 	r ,err:= c.Speaking(ctx,&messagepb.SpeakRequest{MyRequest:name})
-
-// 	Panicking("error ",err)
-// 	log.Println("greeting :",r.GetMyResponse())
-
-// }
-
-var (
-	serverAddr         = flag.String("server_addr", "127.0.0.1:50051", "The server address in the format of host:port")
-	serverHostOverride = flag.String("server_host_override", "", "")
-	insecure           = flag.Bool("insecure", false, "Set to true to skip SSL validation")
-	skipVerify         = flag.Bool("skip_verify", false, "Set to true to skip server hostname verification in SSL validation")
-)
 
 func main() {
-	flag.Parse()
-
-	var opts []grpc.DialOption
-	if *serverHostOverride != "" {
-		opts = append(opts, grpc.WithAuthority(*serverHostOverride))
-	}
-	if *insecure {
-		opts = append(opts, grpc.WithInsecure())
-	} else {
-		cred := credentials.NewTLS(&tls.Config{
-			InsecureSkipVerify: *skipVerify,
-		})
-		opts = append(opts, grpc.WithTransportCredentials(cred))
-	}
-	conn, err := grpc.Dial(*serverAddr, opts...)
-	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
-	}
+	
+	conn, err := grpc.Dial("grpc-knative-cluster1.knative-serving.svc.cluster.local:50051", grpc.WithInsecure(), grpc.WithBlock())
+	logs.Error("Error in connecting", err)
 	defer conn.Close()
-	// conn, err := grpc.Dial("grpc-knative-cluster1.knative-serving.example.com:50051", grpc.WithInsecure(), grpc.WithBlock())
-	// logs.Error("Error in connecting", err)
-	// defer conn.Close()
-	//fmt.Printf("%T",conn)
+	fmt.Printf("%T\n",conn)
 	ClientConnection(conn)
 
 }
@@ -91,6 +44,7 @@ func main() {
 func ClientConnection(conn *grpc.ClientConn) {
 
 	c := messagepb.NewConversationClient(conn)
+    fmt.Println("🚀ClientConnection  : ", c)
 	
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
